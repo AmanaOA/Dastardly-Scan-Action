7b30b54d9bdd4c3933b3a1a77633478404203b43
amanaarmantrading <amanaarmantrading@localhost>
2023-11-07 11:53:24 +0000

    First Commit

---
+++ src/main/java/com/mycompany/myapp1/MyApp1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myapp1;
+
+/**
+ *
+ * @author amanaarmantrading
+ */
+public class MyApp1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
